066a3ce103dcff63d51129aa2f0ccefa3a4e2362
Pandurang <pandurang.plw@gmail.com>
2013-11-07 18:05:23 +0530

    new methods added

--- lib/cloudconvert/conversion.rb
+++ lib/cloudconvert/conversion.rb
@@ -6,19 +6,39 @@ module Cloudconvert
     		@conn = Connection.new(:api_key => api_key)
     	end
 
-    	def convert_file(inputformat, outpuformat, file_path)
-    		response = request(inputformat,outpuformat)
-    		parsed_response = parse_response(response.body)
+    	def convert(inputformat, outpuformat, file_path, options = [])
+    		response = conversion_post_request(inputformat,outpuformat)
+    		
+            parsed_response = parse_response(response.body)
     		@convert_request_url = parsed_response["host"]
     		@process_id = parsed_response["id"]
+
+            #initiate connection with new response host
     		initiate_connection(@convert_request_url)
 
+            upload_params = build_upload_params(file_path, outputformat, email, callback, options)
+            upload(upload_params)
     	end
+
+        def build_upload_params(path, outputformat, email = "0" , callback = nil, options)
+            link = options[:input] == "download" ? 
+            upload_params = { 
+                                :input => "upload",
+                                :file => path,
+                                :outputformat => outputformat,
+                                :email => email,
+                                :options => options
+                            }
+
+            upload_params.merge(:callback => callback) if callback.present?
+            upload_params
+        end
 
     	def initiate_connection(url)
     		@request_connection = Faraday.new(:url => url)
     	end
 
+        #lists all conversions
     	def list_conversions
     		response = @conn.conversion_connection.get '/processes', {:apikey => @conn.api_key } 
     		parse_response(response.body)
@@ -32,12 +52,22 @@ module Cloudconvert
  
     	end
 
-
     	#deletes finished conversion
 		def delete_conversion
     		response = @request_connection.get "/process/"+ @process_id.to_s +"/delete" 
             parse_response(response.body)
     	end
+
+        #upload request
+        def upload(upload_params)
+            response = @request_connection.get "/process/"+ @process_id.to_s, upload_params
+            parse_response(response.body)
+        end
+
+        def status
+            response = @request_connection.get "/process/"+ @process_id.to_s
+            parse_response(response.body)
+        end
 
     	#returns all possible conversions and options
     	def converter_options(inputformat, outputformat)
@@ -49,21 +79,17 @@ module Cloudconvert
 
 
     	#send conversion http request
-    	def conversion_request(path, request_type, inputformat, outputformat)
-    		if request_type == "get"
-    			@conn.conversion_connection.get path, {:inputformat => inputformat,:outputformat => outputformat, :apikey => @conn.api_key } 
-    		elsif request_type == "post"
-    			@conn.conversion_connection.post path, {:inputformat => inputformat,:outputformat => outputformat, :apikey => @conn.api_key } 
-    		end
+    	def conversion_post_request(path, inputformat, outputformat)
+    		@conn.conversion_connection.post path, {:inputformat => inputformat,:outputformat => outputformat, :apikey => @conn.api_key } 
     	end
+
+        def conversion_get_request(path, inputformat, outputformat)
+            @conn.conversion_connection.get path, {:inputformat => inputformat,:outputformat => outputformat, :apikey => @conn.api_key } 
+        end
 
     	#send request http request
     	def send_request(path, request_type, inputformat, outputformat)
-    		if request_type == "get"
-    			@request_connection.get path, {:inputformat => inputformat,:outputformat => outputformat, :apikey => @conn.api_key } 
-    		elsif request_type == "post"
-    			@request_connection.post path, {:inputformat => inputformat,:outputformat => outputformat, :apikey => @conn.api_key } 
-    		end
+    		
     	end
 
     	def parse_response(response)
