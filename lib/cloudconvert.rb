44c11aa4724d752874426d05a8468615ac0ad537
Pandurang <pandurang.plw@gmail.com>
2013-11-11 18:43:51 +0530

    file upload added

--- lib/cloudconvert.rb
+++ lib/cloudconvert.rb
@@ -4,4 +4,6 @@ module Cloudconvert
   # Your code goes here...
   CONVERSION_URL = "https://api.cloudconvert.org/"
 
+  API_KEY_ERROR = "API Key cant be blank!"
+
 end
--- lib/cloudconvert/conversion.rb
+++ lib/cloudconvert/conversion.rb
@@ -3,15 +3,15 @@ module Cloudconvert
     	attr_accessor :convert_request_url, :conn , :request_connection, :process_id
 
     	def initialize(api_key)
-    		@conn = Connection.new(:api_key => api_key)
+            if api_key.present? 
+    		  @conn = Connection.new(:api_key => api_key)
+            else
+                raise Cloudconvert::API_KEY_ERROR
+            end
     	end
 
-    	def convert(inputformat, outpuformat, file_path, options = [])
-    		response = conversion_post_request(inputformat,outpuformat)
-    		
-            parsed_response = parse_response(response.body)
-    		@convert_request_url = parsed_response["host"]
-    		@process_id = parsed_response["id"]
+    	def convert(inputformat, outpuformat, file_path, callback , options = [])
+            @convert_request_url = start_conversion(inputformat, outpuformat)
 
             #initiate connection with new response host
     		initiate_connection(@convert_request_url)
@@ -20,13 +20,21 @@ module Cloudconvert
             upload(upload_params)
     	end
 
-        def build_upload_params(path, outputformat, email = "0" , callback = nil, options)
-            link = options[:input] == "download" ? 
+        def start_conversion(inputformat, outputformat)
+            response = conversion_post_request(inputformat,outpuformat)
+            parsed_response = parse_response(response.body)
+            parsed_response["host"]
+        end
+
+        def initiate_connection(url)
+            @request_connection = Faraday.new(:url => url)
+        end
+
+        def build_upload_params(path, outputformat, callback = nil, options)
             upload_params = { 
                                 :input => "upload",
                                 :file => path,
                                 :outputformat => outputformat,
-                                :email => email,
                                 :options => options
                             }
 
@@ -34,9 +42,18 @@ module Cloudconvert
             upload_params
         end
 
-    	def initiate_connection(url)
-    		@request_connection = Faraday.new(:url => url)
-    	end
+        def build_link_params(link, outputformat, callback = nil, options)
+            upload_params = { 
+                                :input => "download",
+                                :link => link,
+                                :outputformat => outputformat,
+                                :options => options
+                            }
+
+            upload_params.merge(:callback => callback) if callback.present?
+            upload_params
+        end
+
 
         #lists all conversions
     	def list_conversions
@@ -75,22 +92,14 @@ module Cloudconvert
             parse_response(response.body)
     	end
 
-     
-
-
     	#send conversion http request
     	def conversion_post_request(path, inputformat, outputformat)
-    		@conn.conversion_connection.post path, {:inputformat => inputformat,:outputformat => outputformat, :apikey => @conn.api_key } 
+    		@conn.conversion_connection.post "/process", {:inputformat => inputformat,:outputformat => outputformat, :apikey => @conn.api_key } 
     	end
 
         def conversion_get_request(path, inputformat, outputformat)
             @conn.conversion_connection.get path, {:inputformat => inputformat,:outputformat => outputformat, :apikey => @conn.api_key } 
         end
-
-    	#send request http request
-    	def send_request(path, request_type, inputformat, outputformat)
-    		
-    	end
 
     	def parse_response(response)
     		JSON.parse(response)
