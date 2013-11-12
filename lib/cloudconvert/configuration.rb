09f08cfb79e2be90ad7d25fd91e42c95b784f177
Pandurang <pandurang.plw@gmail.com>
2013-11-12 15:14:44 +0530

    added config settings

--- lib/cloudconvert/configuration.rb
+++ lib/cloudconvert/configuration.rb
@@ -1,4 +1,20 @@
+
 module Cloudconvert
   class Configuration
+    attr_accessor :api_key
+
+    def initialize
+      api_key = nil
+    end
+    
+  end
+
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration) if block_given?
   end
 end
--- lib/cloudconvert/conversion.rb
+++ lib/cloudconvert/conversion.rb
@@ -1,11 +1,17 @@
 module Cloudconvert
 	class Conversion
-    	attr_accessor :convert_request_url, :conn , :request_connection, :process_id
+    	attr_accessor :convert_request_url, :conn , :request_connection, :process_id, :conversion_connection
 
-    	def initialize(api_key = nil)
-            raise Cloudconvert::API_KEY_ERROR if api_key == nil
-    		@conn = Connection.new(:api_key => api_key)
+    	def initialize
+            raise Cloudconvert::API_KEY_ERROR if Cloudconvert.configuration.api_key == nil
+    		@conversion_connection = Faraday.new(:url => Cloudconvert::CONVERSION_URL)
+
     	end
+
+        def api_key
+            raise Cloudconvert::API_KEY_ERROR if Cloudconvert.configuration.api_key == nil
+            Cloudconvert.configuration.api_key
+        end
 
 
         #file[:url] =>  input=download
@@ -15,10 +21,8 @@ module Cloudconvert
             @convert_request_url = start_conversion(inputformat, outputformat)
             #initiate connection with new response host
     		initiate_connection(@convert_request_url)
-            binding.pry
-            upload_params = build_upload_params(file, outputformat, callback, options)
 
-            upload(upload_params)
+            upload(build_upload_params(file, outputformat, callback, options))
     	end
 
         def start_conversion(inputformat, outputformat)
@@ -26,6 +30,7 @@ module Cloudconvert
 
             parsed_response = parse_response(response.body)
             @process_id = parsed_response["id"]
+
             "https://#{parsed_response['host']}"
         end
 
@@ -38,26 +43,26 @@ module Cloudconvert
             upload_params = { :format => outputformat, :options => options}
             upload_params.merge(:callback => callback) if callback != nil
             upload_params.merge(:input => "upload",:file => file[:url] ) if file[:url] != nil
-            upload_params.merge(:input => "download",:file => file[:path] ) if file[:url] != nil
+            upload_params.merge(:input => "download",:file => file[:path] ) if file[:url] == nil
             upload_params
         end
 
         #lists all conversions
     	def list_conversions
-    		response = @conn.conversion_connection.get '/processes', {:apikey => @conn.api_key[:api_key] } 
-    		parse_response(response.body)
+    	   response = @conversion_connection.get '/processes', {:apikey => api_key } 
+    	   parse_response(response.body)
     	end
     		
 
     	#cancels current conversion
-    	def cancel_conversion(process_id)
-    	   response = @request_connection.get "/process/#{process_id.to_s}/cancel"
+    	def cancel_conversion
+    	   response = @request_connection.get "/process/#{@process_id.to_s}/cancel"
            parse_response(response.body)
     	end
 
     	#deletes finished conversion
 		def delete_conversion
-    		response = @request_connection.get "/process/#{process_id.to_s}/delete" 
+    		response = @request_connection.get "/process/#{@process_id.to_s}/delete" 
             parse_response(response.body)
     	end
 
@@ -69,31 +74,27 @@ module Cloudconvert
 
 
         # checks if conversion finished for process id and returns download link
-        def download_link(process_id)
-            response = status(process_id)
+        def download_link
+            response = status
             response["step"] == "finished" ? "http:#{response['output']['url']}" : nil
         end 
 
         # checks status of conversion with process_id
-        def status(process_id)
-            response = @request_connection.get "/process/"+ process_id.to_s
+        def status
+            response = @request_connection.get "/process/#{@process_id.to_s}"
             parse_response(response.body)
         end
 
     	#returns all possible conversions and options
     	def converter_options(inputformat, outputformat)
-    		response = @conn.conversion_connection.get "conversiontypes", {:inputformat => inputformat,:outputformat => outputformat } 
+    		response = @conversion_connection.get "conversiontypes", {:inputformat => inputformat,:outputformat => outputformat } 
             parse_response(response.body)
     	end
 
     	#send conversion http request
     	def conversion_post_request(inputformat, outputformat)
-            @conn.conversion_connection.post "https://api.cloudconvert.org/process?inputformat=#{inputformat}&outputformat=#{outputformat}&apikey=#{@conn.api_key[:api_key]}"
+            @conversion_connection.post "https://api.cloudconvert.org/process?inputformat=#{inputformat}&outputformat=#{outputformat}&apikey=#{api_key}"
     	end
-
-        def conversion_get_request(path, inputformat, outputformat)
-            @conn.conversion_connection.get path, {:inputformat => inputformat,:outputformat => outputformat, :apikey => @conn.api_key[:api_key] } 
-        end
 
     	def parse_response(response)
     		JSON.parse(response)
